6efb1e4dd0b80649b96c624959310ffaf6231bea
Aravind Narayanan <39899846+AravattaN@users.noreply.github.com>
2019-11-25 21:15:39 +0530

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,248 @@
+[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "reg_getname",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "fn",
+				"type": "string"
+			},
+			{
+				"name": "lame",
+				"type": "string"
+			},
+			{
+				"name": "rn",
+				"type": "string"
+			},
+			{
+				"name": "mod",
+				"type": "string"
+			},
+			{
+				"name": "chno",
+				"type": "string"
+			},
+			{
+				"name": "adds",
+				"type": "string"
+			},
+			{
+				"name": "pin",
+				"type": "string"
+			}
+		],
+		"name": "reg_setname",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "pol_getname",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "fn",
+				"type": "string"
+			},
+			{
+				"name": "lame",
+				"type": "string"
+			},
+			{
+				"name": "rn",
+				"type": "string"
+			},
+			{
+				"name": "mod",
+				"type": "string"
+			},
+			{
+				"name": "chno",
+				"type": "string"
+			},
+			{
+				"name": "doi",
+				"type": "string"
+			},
+			{
+				"name": "doe",
+				"type": "string"
+			}
+		],
+		"name": "pol_setname",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "fn",
+				"type": "string"
+			},
+			{
+				"name": "lame",
+				"type": "string"
+			},
+			{
+				"name": "rn",
+				"type": "string"
+			},
+			{
+				"name": "mod",
+				"type": "string"
+			},
+			{
+				"name": "chno",
+				"type": "string"
+			},
+			{
+				"name": "doi",
+				"type": "string"
+			},
+			{
+				"name": "doe",
+				"type": "string"
+			}
+		],
+		"name": "insu_setname",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "insu_getname",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+]
